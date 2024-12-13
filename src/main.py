dce7a7bc40061a0ff718decd28942fc21f182215
kedz-1 <khadar7@hotmail.co.uk>
2024-12-13 15:30:29 +0000

    Added functionality so that a new key would be created

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from src.obfuscator import obfuscate_tool
+from src.obfuscator import obfuscation_tool
 from src.s3_utils import read_s3
 from src.s3_utils import write_s3
 
@@ -14,13 +14,19 @@ def obfuscated_data(input_data, region='eu-west-2'):
     s3_key = paths[1]
 
     content = read_s3(file_path, region)
-    obfuscated_content = obfuscate_tool(content, pii)
-    write_s3(s3_bucket, s3_key, obfuscated_content)
+    obfuscated_content = obfuscation_tool(content, pii)
+    new_key = s3_key.replace('.csv', '-masked.csv')
+    write_s3(s3_bucket, new_key, obfuscated_content)
 
     return obfuscated_content
 
+# print(obfuscated_data({
+# "file_to_obfuscate": "s3://masking-test-bucket/masking-test-object",
+# "pii_fields": ["name"]
+# }))
+
 print(obfuscated_data({
-"file_to_obfuscate": "s3://masking-test-bucket/masking-test-object",
-"pii_fields": ["email"]
+"file_to_obfuscate": "s3://final-obfuscation-bucket/final-obfuscation-key.csv",
+"pii_fields": ["name"]
 }))
 
